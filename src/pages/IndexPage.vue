147c227054c3ee247306ecff027166ac25872ba8
staticxinxin <1020912690@qq.com>
2024-01-17 20:16:25 +0800

    feat: 完成本地化ocr

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,26 +1,57 @@
 <script setup lang="ts">
-// useRoute, useHead, and HelloWorld are automatically imported. See vite.config.ts for details.
-const route = useRoute()
+import { createWorker } from "tesseract.js";
 
-useHead({
-  title: route.meta.title,
-  meta: [
-    {
-      property: 'og:title',
-      content: route.meta.title,
-    },
-    {
-      name: 'twitter:title',
-      content: route.meta.title,
-    },
-  ],
-})
+const imgPreview = ref('');
+const handleChangeImg = (e: Event) => {
+  const file = e?.target?.files?.[0];
+  // 只选择图片文件
+  if (!file.type.match('image.*')) {
+    return false;
+  }
 
-const VERSION = import.meta.env.VITE_APP_VERSION
-const BUILD_DATE = import.meta.env.VITE_APP_BUILD_EPOCH
-  ? new Date(Number(import.meta.env.VITE_APP_BUILD_EPOCH))
-  : undefined
-const thisYear = new Date().getFullYear()
+  const reader = new FileReader();
+
+  reader.readAsDataURL(file); // 读取文件
+
+  // 渲染文件
+  reader.onload = function(arg) {
+    imgPreview.value = arg.target?.result as string ?? '';
+    translate(file);
+  }
+}
+
+// 输出文案
+const output = ref('');
+const loggerData = ref<Tesseract.LoggerMessage>({
+  jobId: "",
+  progress: 0,
+  status: "unload",
+  userJobId: "",
+  workerId: ""
+});
+const progress = computed(() => {
+  const num = loggerData.value?.progress.toFixed(2) ?? 0;
+  return Number(num) * 100 + '%';
+});
+const worker = ref<Tesseract.Worker>();
+
+async function initWorker() {
+  worker.value = await createWorker('eng+chi_sim', 1, {
+    workerPath: '/tesseract/worker/worker.min.js',
+    // 下载其他语言训练文件 https://tessdata.projectnaptha.com/[lang].traineddata.gz
+    langPath: '/tesseract/lang',
+    corePath: '/tesseract/core',
+    logger: m => loggerData.value = m, // Add logger here
+  });
+  
+}
+
+initWorker();
+
+async function translate(file: File) {
+  const { data: { text } } = await worker.value!.recognize(file);
+  output.value = text;
+}
 </script>
 
 <template>
@@ -31,158 +62,25 @@ const thisYear = new Date().getFullYear()
     <div
       class="container relative max-w-2xl mx-auto bg-white shadow-xl shadow-slate-700/10 ring-1 ring-gray-900/5"
     >
-      <header class="px-4 pt-6 prose-sm md:px-6 md:prose">
-        <h1>Vite + Vue 3 + TypeScript + Tailwind + Playwright Starter Template v{{ VERSION }}</h1>
-        <p class="pb-4 text-xl leading-relaxed tracking-wide text-gray-700">
-          Opinionated, production ready template for Vite and Vue 3. MIT licensed,
-          <a href="https://github.com/Uninen/vite-ts-tailwind-starter">available on GitHub</a>.
-        </p>
-      </header>
       <main>
-        <div class="p-4 mx-auto prose md:px-6 prose-indigo sm:rounded-md">
-          <HelloWorld msg="Hello World Component" />
+        <input type="file" name="selectImg" id="selectImg" @change="handleChangeImg" accept="image/*">
+        <img v-if="imgPreview" :src="imgPreview" width="500" alt="preview">
+        <div v-else class="w-32 h-32 border border-black border-solid flex justify-center items-center mt-2">
+          <span>
+            图片预览
+          </span>
+        </div>
 
-          <h2>Template Project Features</h2>
+        <div class="my-2">
+          输出文案如下：
 
-          <p>
-            The idea of this template is to bundle as many often used and useful features
-            pre-configured as possible. If you don't need something, just remove it!
-          </p>
-
-          <ul>
-            <li
-              >Pinia store (fully typed Vuex store available in
-              <a href="https://github.com/Uninen/vite-ts-tailwind-starter/tree/v1.13"
-                >template version 1.13</a
-              >)</li
-            >
-            <li>Routing using vue-router</li>
-            <li>
-              TailwindCSS w/ JIT-mode, purge, plugins, and production minimization pre-configured
-            </li>
-            <li>
-              Vue-friendly Eslint and Prettier configuration - use as is or tweak to your liking
-            </li>
-            <li>Alias <code>@</code> to <code>project_root/src</code></li>
-            <li>Predefined and typed global variables</li>
-            <li>Playwright e2e and component tests preconfigured</li>
-            <li>
-              Github Workflows preconfigured to run e2e and component tests automatically on every
-              push
-            </li>
-          </ul>
-
-          <h2>Project setup and usage</h2>
-
-          <p>
-            Using
-            <a href="https://github.com/johnsoncodehk/volar">Volar extension for VSCode</a>
-            is recommended to take full advance of the new script setup sugar and full TypeScript
-            support in Vue SFC templates. (This is totally optional but recommended as it results
-            much better DX!) See
-            <a href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
-              >official IDE Support documentation</a
-            >
-            for more details.
-          </p>
-
-          <h4>Install dependencies</h4>
-
-          <pre>pnpm i</pre>
-
-          <h4>Run development server</h4>
-
-          <pre>pnpm dev</pre>
-
-          <h4>Run unit + component tests (Vitest)</h4>
-
-          <pre>pnpm test</pre>
-
-          <h4>Run e2e tests (Playwright)</h4>
-
-          <pre>pnpm test-e2e</pre>
-
-          <h4>Build for production</h4>
-
-          <pre>pnpm build</pre>
-
-          <h4>Other</h4>
-
-          <p>See <code>package.json</code> for all available commands.</p>
-
-          <h2>Notes and further documentation</h2>
-
-          <h3>Typed ENV Variables</h3>
-
-          <p>
-            Vite exposes a special <code>meta.env</code> object for ENV variables (see
-            <a href="https://vitejs.dev/guide/env-and-mode.html">official docs</a>). This template
-            extends that object and adds custom typed variables which you can easily use and modify
-            to your needs.
-          </p>
-
-          <p>
-            See <code>vite.config.js</code> and <code>src/env.d.ts</code> for the configuration and
-            <code>src/pages/BaseTemplate.vue</code> for usage example.
-          </p>
-
-          <h3>Code Coverage</h3>
-
-          <p>
-            TL;DR: due to Vite and esbuild design decisions and ongoing work in this area, code
-            coverage is not yet added to this template.
-          </p>
-
-          <p>
-            I have a fully configured Vue 3 + TypeScript + Cypress + Coverage reporting + GitLab and
-            GitHub CI `vue-cli` template project available at
-            <a href="https://gitlab.com/uninen/vue-cli-template"
-              >https://gitlab.com/uninen/vue-cli-template</a
-            >. That's a real world example of proper coverage configuration that works automatically
-            in CI. But. This setup doesn't work with Vite.
-          </p>
-
-          <p>
-            Typical Vite pipelines don't use babel at all which is needed above for automatically
-            instrument the transpilated code. Vite is powered by eslint which has decided
-            <a href="https://github.com/evanw/esbuild/issues/184">
-              code coverage being out of scope </a
-            >. Vitest can be configured to produce code coverage as well but that only counts unit
-            tests.
-          </p>
-
-          <h2>Elsewhere</h2>
-
-          <ul>
-            <li>Follow <a href="https://twitter.com/uninen">@Uninen on Twitter</a></li>
-            <li>
-              Lots of continuously updating learnings from Vite / Vue / TypeScript and other Web
-              development topics on my
-              <a href="https://til.unessa.net/">Today I Learned</a> site
-            </li>
-          </ul>
-
-          <h2>Contributing</h2>
-
-          <p>
-            Contributions are welcome! Please follow
-            <a href="https://www.contributor-covenant.org/version/2/0/code_of_conduct/">
-              the code of conduct
-            </a>
-            when interacting with others.
-          </p>
+          <pre v-if="output">{{ output }}</pre>
+          <div v-else>
+            OCR组件状态 {{ loggerData?.status }}
+            {{ progress }}
+          </div>
         </div>
       </main>
-      <footer class="py-6 text-sm text-center text-gray-700">
-        <p>
-          Vite-ts-tailwind-starter by
-          <a class="underline" href="https://twitter.com/uninen">@Uninen</a> &copy; 2020-{{
-            thisYear
-          }}.
-          <template v-if="BUILD_DATE"> Site built {{ BUILD_DATE.toLocaleDateString() }}. </template>
-          <template v-else> Development mode. </template>
-        </p>
-      </footer>
     </div>
   </div>
 </template>
